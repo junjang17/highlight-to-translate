1a4118083a4658da4c983c35cdc5677c80bca513
Jun Jang <junjang@college.harvard.edu>
2018-08-15 00:12:32 -0500

    updated readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Highlight-To-Translate
+Highlight-To-Translate is a Chrome extension that allows users to translate text on web pages by highlighting the text. 
+This tool is especially useful for language learners who need quick, convenient translations of a few words or phrases
+rather than the whole text.
+
+
+## Usage
+Select the desired target language (default is English). Once selected, hit apply.
+
+![](/Users/junjang/Desktop/readme pics/Screen Shot 2018-08-14 at 8.42.23 PM.png)
+
+Highlight the word, phrase, or paragraph to be translated to see your translation!
+
+![Screen Shot 2018-08-14 at 8.43.37 PM](/Users/junjang/Desktop/readme pics/Screen Shot 2018-08-14 at 8.43.37 PM.png)
+
+Once the translation is visible, click anywhere else on the screen to remove the popup.
+
+
+
+This extension is powered using the Yandex Translation API. Information can be found at <http://translate.yandex.com/>.
--- content.js
+++ content.js
@@ -3,9 +3,17 @@ var script = document.createElement("script");
 script.src = "jquery-3.3.1.min.js"
 
 // Create popup element to load with text, add to bottom of body
-var translate_popup = document.createElement("span");
+var translate_popup = document.createElement("div");
 translate_popup.className = "popup-tag";
 document.body.appendChild(translate_popup);
+
+// Add sponsor text to results
+var sponsor = document.createElement("a");
+sponsor.href = "http://translate.yandex.com/";
+var sponsor_txt = document.createTextNode("Powered by Yandex.Translate");
+sponsor.appendChild(sponsor_txt);
+
+
 
 // Function selects text from DOM
 function getSelectionText() {
@@ -31,16 +39,16 @@ chrome.runtime.onMessage.addListener(
 document.addEventListener("mouseup", function(event){
 	t = getSelectionText();
     if (t == ""){
-        $("span.popup-tag").css("display","none");
+        $("div.popup-tag").css("display","none");
     }
 
 	chrome.runtime.sendMessage({input: t, lang: lang_code}, function(response) {
         selection = response.message
         if(selection != ''){
-            $("span.popup-tag").css("display","block");
-            $("span.popup-tag").css("top",event.pageY);
-            $("span.popup-tag").css("left",event.pageX);
-            $("span.popup-tag").text(selection);
+            $("div.popup-tag").css("display","block");
+            $("div.popup-tag").css("top",event.pageY);
+            $("div.popup-tag").css("left",event.pageX);
+            $("div.popup-tag").text(selection);
         }
 
 	});
